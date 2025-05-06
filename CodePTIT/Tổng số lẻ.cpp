fc96dd86a09e16f02713092aaa1e28373fe41745
TuilaTammmmm <dragonkiller2k5@gmail.com>
2025-05-06 13:20:53 +0700

    code

---
+++ CodePTIT/Tổng số lẻ.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    long long sum = 0;
+    for(long long i = 1; i <= n; i+=2) {
+        sum += i;
+    }
+    cout << sum << endl;
+    return 0;
+}
